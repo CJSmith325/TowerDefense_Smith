80f687d3280934dfdb7aff9dacc79037ef76aac4
CJSmith325 <csmith14@ltu.edu>
2022-03-17 23:15:09 -0400

    More Additions

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl
+{
+    public static int currency = 0;
+    public static int health = 200;
+
+
+}
