7a0874073cac6a9adaf79738cbda13a31232f9ea
CJSmith325 <csmith14@ltu.edu>
2022-03-19 16:52:51 -0400

    Targeting

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,5 +4,54 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    
+    private Transform target;
+
+    public float range = 20f;
+
+
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        float shortestDist = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDist)
+            {
+                shortestDist = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDist <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
 }
